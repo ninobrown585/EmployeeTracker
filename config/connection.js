9877c4316150c93d84a87ddcde71c6d380a47837
Nino Brown <ninobrown58518@gmail.com>
2022-10-25 21:59:00 -0500

    added connection.js

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql2");
+
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "employee_trackerDB"
+});
+
+
+// Initiate MySQL Connection.
+connection.connect(function(err){
+    if(err){
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+    
+})
+
+module.exports = connection;
---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department(
+	id INT AUTO_INCREMENT, 
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE role(
+	id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(9,2),
+    department_id INTEGER,
+    PRIMARY KEY (id),
+    CONSTRAINT dep_fk FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE employee(
+	id INT AUTO_INCREMENT,
+	first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT role_fk FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT manager_fk FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
